6e7b460ade598505eae92c2ff8ad8fff5a1ff097
Jorgedavyd <jorged.encyso@gmail.com>
2024-07-10 17:20:11 -0400

    adding scripts

---
+++ scripts/requirements.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+python3 -m pip install --upgrade pip
+pip3 install pytest black pipreqs pip-tools
+pipreqs starstream --savepath=./requirements.in
+sed -i 's/==.*$//' requirements.in
+sort requirements.in | uniq > requirements_unique.in
+pip-compile ./requirements_unique.in
+rm -f *.in
+pip install -r requirements_unique.txt
+rm -f requirements_unique.txt
+python3 update.py
