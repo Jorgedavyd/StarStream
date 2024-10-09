7002868f647f74e527391bfc88caafb9258e9b29
Jorgedavyd <jorged.encyso@gmail.com>
2024-10-09 18:29:08 -0300

    downloader

---
+++ starstream/downloader.py
@@ -0,0 +1,23 @@
+from typing import List, Union, Tuple
+from datetime import datetime
+from ._base import Satellite
+import asyncio
+import aiohttp
+
+def DataDownloading(
+    sat_objs: Union[List, Satellite],
+    scrap_date: Union[List[Tuple[datetime, datetime]], Tuple[datetime, datetime]],
+) -> None:
+    if not isinstance(sat_objs, list):
+        sat_objs = [sat_objs]
+    asyncio.run(downloader(sat_objs, scrap_date))
+
+
+async def downloader(
+    sat_objs: List[Satellite],
+    scrap_date: Union[List[Tuple[datetime, datetime]], Tuple[datetime, datetime]],
+) -> None:
+    async with aiohttp.ClientSession() as session:
+        await asyncio.gather(
+            *[satellite.fetch(scrap_date, session) for satellite in sat_objs]
+        )
