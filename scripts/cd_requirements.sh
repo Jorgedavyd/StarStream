a513fad77ab1bbb17a71e9d261148a5669e1b326
Jorgedavyd <jorged.encyso@gmail.com>
2024-12-20 03:39:43 -0300

    pip troubleshoot

--- scripts/cd_requirements.sh
+++ scripts/cd_requirements.sh
@@ -6,6 +6,6 @@ sed -i 's/==.*$//' requirements.in
 sort requirements.in | uniq > requirements_unique.in
 pip-compile ./requirements_unique.in
 rm -f *.in
-pip install -r requirements_unique.txt
+pip install -r requirements_unique.txt --ignore-installed
 rm -f requirements_unique.txt
 playwright install
